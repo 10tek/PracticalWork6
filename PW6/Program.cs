17d492eda5ee6526b92952b1df762054250cf32c
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-09-28 19:30:20 +0600

    Добавлен класс-массив в котором индексный диапазон устанавливается пользователем. Демонстрация работы в Main.

---
+++ PW6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PW6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var startIndex = -2;
+            var endIndex = 1;
+            Random random = new Random();
+            RangeOfArray array = new RangeOfArray(startIndex, endIndex);
+
+            for (var i = startIndex; i < endIndex; i++)
+            {
+                array[i] = random.Next(0, 10);
+            }
+
+            for (var i = startIndex; i < endIndex; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            array[2] = 23;
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PW6/RangeOfArray.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PW6
+{
+    class RangeOfArray
+    {
+        private int[] _array;
+
+        public int Lenght { get; set; }
+        public int EndIndex { get; set; }
+        public int BeginIndex { get; set; }
+
+        public RangeOfArray(int beginIndex, int endIndex)
+        {
+            EndIndex = endIndex;
+            BeginIndex = beginIndex;
+            Lenght = Math.Abs(endIndex - beginIndex);
+            _array = new int[Lenght];
+        }
+
+
+        public int this[int index]
+        {
+            get
+            {
+                try
+                {
+                    if (index < BeginIndex || index >= EndIndex)
+                    {
+                        throw new Exception("Out of range");
+                    }
+                    return _array[index - BeginIndex];
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"ERROR! {exception.Message}");
+                    return -1;
+                }
+            }
+            set
+            {
+                try
+                {
+                    if (index < BeginIndex || index >= EndIndex)
+                    {
+                        throw new Exception("Out of range");
+                    }
+                    _array[index - BeginIndex] = value;
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"ERROR! {exception.Message}");
+                }
+            }
+        }
+    }
+}
